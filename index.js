775f6c46ff5bc50edf8530d5405d6838a4d5f322
G.V.Rasmika <160033602+gvrasmika@users.noreply.github.com>
2024-09-22 11:33:51 +0530

    Add files via upload
    rock-paper-scissors game

---
+++ index.js
@@ -0,0 +1,48 @@
+const choices=["ROCK","PAPER","SCISSORS"];
+const playerDisplay =document.getElementById("playerDisplay");
+const computerDisplay =document.getElementById("computerDisplay");
+const resultDisplay =document.getElementById("resultDisplay");
+const playerScoreDisplay =document.getElementById("playerScoreDisplay");
+const computerScoreDisplay =document.getElementById("computerScoreDisplay");
+let playerScore=0;
+let computerScore=0;
+
+function playGame(playerChoice){
+    const computerChoice=choices[Math.floor(Math.random() * 3)];
+    let result="";
+    if(playerChoice === computerChoice){
+        result="IT'S A TIE";
+    }
+    else{
+        switch(playerChoice){
+            case "ROCK":
+                result=(computerChoice==="SCISSORS")? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "PAPER":
+                result=(computerChoice==="ROCK")? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "SCISSORS":
+                result=(computerChoice==="PAPER")? "YOU WIN!" : "YOU LOSE!";
+                break;
+        }
+    }
+    playerDisplay.textContent=`PLAYER: ${playerChoice}`;
+    computerDisplay.textContent=`COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent=result;
+
+    resultDisplay.classList.remove("greenText","redText");
+
+    switch(result){
+        case"YOU WIN!":
+         resultDisplay.classList.add("greenText");
+         playerScore++;
+         playerScoreDisplay.textContent=playerScore;
+         break;
+
+         case"YOU LOSE!":
+         resultDisplay.classList.add("redText");
+         computerScore++;
+         computerScoreDisplay.textContent=computerScore;
+         break;
+    }
+}
